8b6f3cbc951cb6aaed5302cab02bb8d6c81b5601
enzinius <enzo.colaiezzi@gmail.com>
2025-09-22 19:16:30 +0200

    Adds test suite for products and in-stock notification to subscribers

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include ActionMailer::TestHelper
+
+  test "sends email to subscribers when product is back in stock" do
+    product = products(:tshirt)
+
+    # Set product inventory to 0 to simulate out of stock
+    product.update(inventory_count: 0)
+
+    assert_emails 2 do
+      product.update(inventory_count: 99)
+    end
+  end
 end
